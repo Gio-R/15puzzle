3c07f1895203297a14d68613a108f4f5573c4b24
Gio-R <>
2021-09-10 00:07:47 +0200

    Fix sovlable puzzle and puzzle with empty tiles creation, add tests

--- src/main/scala/model/Puzzle.scala
+++ src/main/scala/model/Puzzle.scala
@@ -2,6 +2,9 @@ package model
 
 class Puzzle (private val tiles: Map[(Int, Int), Tile]):
   private val size = scala.math.sqrt(tiles.size).toInt
+  require(tiles.size > 3)
+  require(scala.math.sqrt(tiles.size).isValidInt)
+  require(tiles.find(_._2 == Empty()).map(_._1._1).isDefined)
 
 object Puzzle:
   def createRandomPuzzle(puzzleSize: Int): Puzzle =
@@ -17,16 +20,18 @@ object Puzzle:
     val tiles = (1 to tileMaxNumber).map(Number(_))
     val map = scala.collection.mutable.Map[(Int, Int), Tile]()
     var emptyPositioned = false
-    for 
-      i <- 1 to size
-      j <- 1 to size
-    do
-      val tile: Tile = if !emptyPositioned && scala.util.Random.nextBoolean() then
-        emptyPositioned = true
-        Empty()
-      else
-        tiles(0)
-      map.addOne(((i, j), tile))
+    while (!emptyPositioned)
+      map.clear()
+      for 
+        i <- 1 to size
+        j <- 1 to size
+      do
+        val tile: Tile = if !emptyPositioned && scala.util.Random.nextBoolean() then
+          emptyPositioned = true
+          Empty()
+        else
+          tiles(0)
+        map.addOne(((i, j), tile))
     map.toMap
 
   def isPuzzleSolvable(puzzle: Puzzle): Boolean =
@@ -40,11 +45,12 @@ object Puzzle:
     val orderedNumberedTiles = orderedTiles.filter(_.isInstanceOf[Number]).map(_.asInstanceOf[Number])
     var inversions = 0
     for
-      i <- 0 to orderedNumberedTiles.size - 1
-      j <- 0 to orderedNumberedTiles.size - 1
+      i <- 0 to orderedNumberedTiles.size - 2
+      j <- i + 1 to orderedNumberedTiles.size - 1
       if orderedNumberedTiles(i).n > orderedNumberedTiles(j).n
     do
       inversions += 1
+    // println(s"puzzle size ${puzzle.size}; inversions $inversions; emtpyTileRow $emptyTileRow")
     ((puzzle.size % 2 != 0 && inversions % 2 == 0)
       || (puzzle.size % 2 == 0 && (
         (inversions % 2 == 0 && emptyTileRow.forall(_ % 2 == 0))
--- src/test/scala/model/PuzzleTest.scala
+++ src/test/scala/model/PuzzleTest.scala
@@ -1,41 +1,70 @@
 package model
 
 class PuzzleSuite extends munit.FunSuite:
-    test("Size less than 4 is not allowed") {
-      intercept[java.lang.IllegalArgumentException](Puzzle.createRandomPuzzle(3))
-    }
+  test("Size less than 4 is not allowed") {
+    intercept[java.lang.IllegalArgumentException](Puzzle.createRandomPuzzle(3))
+  }
 
-    test("Size can not be negative") {
-      intercept[java.lang.IllegalArgumentException](Puzzle.createRandomPuzzle(-1)) 
-    }
+  test("Size can not be negative") {
+    intercept[java.lang.IllegalArgumentException](Puzzle.createRandomPuzzle(-1)) 
+  }
 
-    test("Size must be a perfect square") {
-      Puzzle.createRandomPuzzle(4)
-      Puzzle.createRandomPuzzle(9)
-      intercept[java.lang.IllegalArgumentException](Puzzle.createRandomPuzzle(5)) 
-    }
+  test("Size must be a perfect square") {
+    Puzzle.createRandomPuzzle(4)
+    Puzzle.createRandomPuzzle(9)
+    intercept[java.lang.IllegalArgumentException](Puzzle.createRandomPuzzle(5)) 
+  }
 
-    test("Solvable puzzle is recognised as such") {
-      // val puzzle = Puzzle(Map((1, 1), Number(13)),
-      //                         (1, 2), Number(13)),
-      //                         (1, 3), Number(13)),
-      //                         (1, 4), Number(13)),
-      //                         (2, 1), Number(13)),
-      //                         (2, 2), Number(13)),
-      //                         (2, 3), Number(13)),
-      //                         (2, 4), Number(13)),
-      //                         (3, 1), Number(13)),
-      //                         (3, 2), Number(13)),
-      //                         (3, 3), Number(13)),
-      //                         (3, 4), Number(13)),
-      //                         (4, 1), Number(13)),
-      //                         (4, 2), Number(13)),
-      //                         (4, 3), Number(13)),
-      //                         (4, 4), Number(13)),
-      //                        )
-    }
+  test("Solvable puzzle is recognised as such") {
+    val puzzle = Puzzle(Map(((1, 1), Number(13)),
+                            ((1, 2), Number(2)),
+                            ((1, 3), Number(10)),
+                            ((1, 4), Number(3)),
+                            ((2, 1), Number(1)),
+                            ((2, 2), Number(12)),
+                            ((2, 3), Number(8)),
+                            ((2, 4), Number(4)),
+                            ((3, 1), Number(5)),
+                            ((3, 2), Empty()),
+                            ((3, 3), Number(9)),
+                            ((3, 4), Number(6)),
+                            ((4, 1), Number(15)),
+                            ((4, 2), Number(14)),
+                            ((4, 3), Number(11)),
+                            ((4, 4), Number(7)),
+                            )
+                        )
+    assert(Puzzle.isPuzzleSolvable(puzzle = puzzle))
+  }
 
-    test("Unsolvable puzzle is recognised as such") {
+  test("Unsolvable puzzle is recognised as such") {
+    val puzzle = Puzzle(Map(((1, 1), Number(3)),
+                            ((1, 2), Number(9)),
+                            ((1, 3), Number(1)),
+                            ((1, 4), Number(15)),
+                            ((2, 1), Number(14)),
+                            ((2, 2), Number(11)),
+                            ((2, 3), Number(4)),
+                            ((2, 4), Number(6)),
+                            ((3, 1), Number(13)),
+                            ((3, 2), Empty()),
+                            ((3, 3), Number(10)),
+                            ((3, 4), Number(12)),
+                            ((4, 1), Number(2)),
+                            ((4, 2), Number(7)),
+                            ((4, 3), Number(8)),
+                            ((4, 4), Number(5)),
+                            )
+                        )
+    assert(!Puzzle.isPuzzleSolvable(puzzle = puzzle))
+  }
 
-    }
+  test("Puzzle without empty tile cannot be created") {
+    intercept[IllegalArgumentException](Puzzle(Map(((1, 1), Number(13)),
+                                                  ((1, 2), Number(2)),
+                                                  ((1, 3), Number(10)),
+                                                  ((1, 4), Number(3)))
+                                              )
+                                        )
+  }
       
